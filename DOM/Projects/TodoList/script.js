dabd6ae04a6ff5d481d7abeb61fe5fced2c30f54
mahmutyoung <54088505+mahmutyoung@users.noreply.github.com>
2022-05-13 00:20:03 +0300

    local Storage implemented

--- DOM/Projects/TodoList/script.js
+++ DOM/Projects/TodoList/script.js
@@ -1,34 +1,54 @@
 const btnAdd = document.querySelector("#btn-add");
-//let myTasks = document.getElementsByClassName("task")
 const myTasks = document.getElementsByClassName("tasks")[0]
 const input = document.querySelector("#input-area");
 const btnDeleteAll = document.getElementsByClassName("btn-delete-all")[0]
 btnDeleteAll.disabled = true;
+let myTasksArray = [];
+let key = "storedTasks"
 
+loadFromStorage();
 
 btnAdd.addEventListener("click", function() {
 
     if (input.value === "") {
         alert("put some text")
     } else {
-        const newTask = document.createElement("li")
-        newTask.classList.add("li1");
-        newTask.innerHTML = `<p>${input.value} </p>` + "<button class='delete'>X</button>"
-
-        myTasks.appendChild(newTask)
-
+        createTask(input.value);
+        myTasksArray.push(input.value)
+        addToLocalStorage(myTasksArray);
         input.value = ""; //after taking input, gives a fresh input
-
-        newTask.children[1].addEventListener("click", function() {
-            this.parentElement.remove();
-        });
-        btnDeleteAll.disabled = false;
 
     }
 });
 
+function createTask(str) {
+    const newTask = document.createElement("li");
+    newTask.className = "li1";
+    newTask.innerHTML = `<p>${str} </p>` + "<button class='delete'>X</button>"
+    myTasks.appendChild(newTask)
+    newTask.children[1].addEventListener("click", function() {
+        this.parentElement.remove();
+        myTasksArray.pop();
+        addToLocalStorage(myTasksArray);
+    });
+    btnDeleteAll.disabled = false;
+}
+
 btnDeleteAll.addEventListener("click", function() {
     myTasks.innerHTML = "";
-
-
+    localStorage.removeItem(key);
 });
+
+function loadFromStorage() {
+    if (localStorage.getItem(key) !== null) {
+        myTasksArray = JSON.parse(localStorage.getItem(key));
+        for (let i = 0; i < myTasksArray.length; i++) {
+            createTask(myTasksArray[i]);
+        }
+    }
+}
+
+function addToLocalStorage(array) {
+    let tasks = JSON.stringify(array)
+    localStorage.setItem(key, tasks)
+}

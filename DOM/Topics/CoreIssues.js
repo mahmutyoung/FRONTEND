853104c3a524fbf0cb61b08228653274fe4d944c
mahmutyoung <54088505+mahmutyoung@users.noreply.github.com>
2022-05-18 13:03:50 +0300

    Update CoreIssues.js

---
+++ DOM/Topics/CoreIssues.js
@@ -0,0 +1,13 @@
+/**Core Topics*/
+
+/**
+ * A. Collections
+ * 
+ * A1.
+ * HTMLCollection and NodeList are array-like but not array.
+ * You can not use array methods.
+ *  
+ * for-loop can be used for HTMLCollection, but not for NodeList.
+ *  
+ * for-of can be used for both.
+ */
